dd8e74cff7d9e6508ea8631b20439e2a1a21cc4f
LovenSadullah <sadullahl@coventry.ac.uk>
2026-01-19 17:06:58 +0000

    main

---
+++ calc.py
@@ -0,0 +1,23 @@
+while True:
+
+    x = int(input("enter your first number:"))
+    y = int(input("enter your second number:")) # input the um 1st & 2nd
+    z = input("what do you want to do with these two numbers?input from -, +, / or *")
+
+    if z == ("-"):   
+        print(x - y)
+
+    if z == ("+") :  
+        print(x + y)
+        
+    if z == ("*") :  
+        print(x * y)
+        
+    if z == ("/") :  
+        print(x / y)
+        
+    break
+        
+
+        
+
